44df61d3d3dbfe57aefaec04423186708da94086
Ogshub <123223272+Ogshub@users.noreply.github.com>
2024-12-12 21:40:58 +0530

    Add files via upload

---
+++ Hangman.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <limits>
+#include "Random.h"
+
+namespace WordList {
+	const std::vector<std::string> wordVector{
+		"luffy","eren","miyamoto","kojiro","thorffin","guts","killua","hisoka","zoro","shanks","mikasa"
+	};
+
+	std::string getRandomWord() {
+		int wordIndex{};
+		wordIndex = Random::get(0, static_cast<int>(wordVector.size() - 1));
+		return wordVector[wordIndex];
+	}
+
+	std::string getHiddenName(const std::string& word) {
+		std::string hidden(word.length(), '_');
+		return hidden;
+	}
+}
+
+class Session {
+	std::string RandomWord{};
+	std::string HiddenWord{};
+	std::vector<char> guessedLetters{};
+	int remAttempts{ 5 };
+public:
+	Session()
+		: RandomWord{ WordList::getRandomWord() }, HiddenWord{WordList::getHiddenName(RandomWord)} {
+
+	}
+
+	void displayWord() {
+		std::cout << "\n";
+		std::cout << "The word is: " << HiddenWord << "\n";
+	}
+
+	void welcome() {
+		std::cout << "Welcome to C++ Hangman Game!.\n";
+		std::cout << "To win: guess the word,\tTo lose: run out of pluses(+).\n";
+		std::cout << "Remaining attempts : " << std::string(remAttempts,'+') << "\n";
+	}
+
+	void getInput() {
+		char input{};
+		while (true) {
+			std::cout << "Enter your guess: ";
+			std::cin >> input;
+			if (std::cin.fail() || !std::isalpha(input)) {
+				std::cin.clear();
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				std::cout << "Please enter valid characters.\n";
+				continue;
+			}
+
+			input = std::tolower(input);
+
+			if (std::find(guessedLetters.begin(), guessedLetters.end(), input) != guessedLetters.end()) {
+				std::cout << "You have already guessed " << input << ". Try a different letter.\n";
+				continue;
+			}
+
+			guessedLetters.push_back(input);
+			break;
+		}
+	}
+
+	bool gameProcess(char& guess) {
+		bool isCorrectGuess{ false };
+		for (std::size_t i{ 0 }; i < RandomWord.length(); ++i) {
+			if (RandomWord[i] == guess) {
+				HiddenWord[i] = guess;
+				isCorrectGuess = true;
+			}
+		}
+
+		if (!isCorrectGuess) {
+			--remAttempts;
+		}
+
+		return isCorrectGuess;
+	}
+
+	bool isGameOver() const {
+		if (HiddenWord == RandomWord) {
+			std::cout << "Congrats You win!.\n";
+			return true;
+		}
+
+		if (remAttempts <= 0) {
+			std::cout << "Too bad you lose.Try again next time!.\n";
+			return true;
+		}
+
+		return false;
+	}
+
+	void gameLoop() {
+		while (!isGameOver()) {
+			displayWord();
+			getInput();
+			
+			char PreviousGuess{guessedLetters.back()};
+
+			if (gameProcess(PreviousGuess)) {
+				std::cout << "Good guess.\n";
+			}
+			else {
+				std::cout << "Incorrect guess!.Remaining attempt: " << std::string(remAttempts, '+') << "\n";
+			}
+		}
+	}
+};
+
+
+int main() {
+	Session s{};
+	s.welcome();
+	s.gameLoop();
+	std::cout << "\n";
+	std::cout << "Thanks for playing!.";
+
+	return 0;
+}
